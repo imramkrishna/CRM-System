f01879a422b99fc426a5ef5bb56d9beb10fbae84
Ram Krishna Yadav <itsramky234@gmail.com>
2025-08-11 20:08:34 +0545

    updated origin path

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,6 +16,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
     cors({
+        origin: process.env.CLIENT_URL || "http://localhost:3000",
         credentials: true,
         methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
